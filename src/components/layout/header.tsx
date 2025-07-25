c2b8c1dd363774da23e23a61b720710c1a3d7cd0
ThaNys <loithienly@gmail.com>
2025-07-25 22:06:35 +0700

    feat: add-ui-login

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -53,101 +53,130 @@ const Header = () => {
               <div className="flex items-center gap-1 cursor-pointer hover:text-blue-400">
                 <Dialog>
                   <DialogTrigger asChild>
-                    <Button type="button">
+                    <div className="flex gap-2 items-center cursor-pointer">
                       <AccountCircleOutline />
                       Đăng nhập
-                    </Button>
+                    </div>
                   </DialogTrigger>
                   <Formik
                     initialValues={{ email: "", password: "" }}
                     onSubmit={(values) => {
                       console.log("Form submitted with values:", values);
                     }}
+                    enableReinitialize
                   >
                     {({ values, setFieldValue, setFieldTouched }) => {
                       console.log("Formik values:", values);
 
                       return (
-                        <DialogContent className="sm:max-w-[425px]">
-                          <DialogTitle>Edit profile</DialogTitle>
-                          <div className="flex justify-center items-center text-xl font-bold">
-                            Đăng nhập / Tạo tài khoản
-                          </div>
-                          <div className="grid gap-4">
-                            <div className="grid gap-3 relative">
-                              <label htmlFor="email">Email</label>
-                              <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                value={values.email}
-                                onChange={(e) => {
-                                  setFieldValue("email", e.target.value);
-                                  setFieldTouched("email", true);
-                                }}
-                                disabled={showPasswordBox}
-                              />
+                        <DialogContent className="sm:max-w-[425px] p-0">
+                          <DialogTitle className="flex justify-center items-center p-1">
+                            <img src="https://s1.vnecdn.net/vnexpress/restruct/i/v9647/v2_2019/pc/graphics/logo_tagline.svg" />
+                          </DialogTitle>
+                          <div className="bg-gray-100 p-4 rounded-md space-y-4">
+                            <div className="flex flex-col justify-center items-center text-xl font-bold">
+                              {!showPasswordBox
+                                ? "Đăng nhập / Tạo tài khoản"
+                                : "Đăng nhập"}
                               {showPasswordBox && (
-                                <div className="absolute right-3 top-1/2 -translate-y-1/2">
-                                  <Button
-                                    onClick={() => setShowPasswordBox(false)}
-                                  >
-                                    <EditPenTextWrite />
-                                  </Button>
+                                <div className="text-sm font-normal text-gray-500">
+                                  Bạn đã có tài khoản trên VnExpress
                                 </div>
                               )}
                             </div>
-                            {showPasswordBox && (
+                            <div className="grid gap-4">
                               <div className="grid gap-3 relative">
-                                <label htmlFor="password">Password</label>
+                                <label htmlFor="email">Email</label>
                                 <input
-                                  id="password"
-                                  name="password"
-                                  type={showPassword ? "text" : "password"}
-                                  value={values.password}
+                                  id="email"
+                                  name="email"
+                                  type="email"
+                                  value={values.email}
                                   onChange={(e) => {
-                                    setFieldValue("password", e.target.value);
-                                    setFieldTouched("password", true);
+                                    setFieldValue("email", e.target.value);
+                                    setFieldTouched("email", true);
                                   }}
+                                  disabled={showPasswordBox}
+                                  className="border border-gray-300 rounded-md p-3 w-full"
                                 />
-                                {values.password && (
-                                  <div className="absolute right-3 top-1/2 -translate-y-1/2">
-                                    <Button
-                                      onClick={() =>
-                                        setShowPassword(!showPassword)
-                                      }
+                                {showPasswordBox && (
+                                  <div className="absolute right-3 bottom-2 p-1 bg-gray-100 cursor-pointer">
+                                    <div
+                                      onClick={() => setShowPasswordBox(false)}
+                                      className="bg-transparent border-none"
                                     >
-                                      {showPassword ? <EyeOff /> : <Eye />}
-                                    </Button>
+                                      <div className="flex items-center gap-1">
+                                        Sửa <EditPenTextWrite />
+                                      </div>
+                                    </div>
                                   </div>
                                 )}
                               </div>
-                            )}
-                          </div>
-                          <div className="flex justify-center">
-                            {!showPasswordBox && (
-                              <Button
-                                variant="primary"
-                                fill
-                                disabled={!values.email}
-                                onClick={() => {
-                                  setShowPasswordBox(true);
-                                  setFieldValue("password", "");
-                                }}
-                              >
-                                Tiếp tục
-                              </Button>
-                            )}
-                            {showPasswordBox && (
-                              <Button
-                                type="submit"
-                                variant="primary"
-                                fill
-                                disabled={!values.password}
-                              >
-                                Đăng nhập
-                              </Button>
-                            )}
+                              {showPasswordBox && (
+                                <div className="grid gap-3 relative">
+                                  <label htmlFor="password">Password</label>
+                                  <input
+                                    id="password"
+                                    name="password"
+                                    type={showPassword ? "text" : "password"}
+                                    value={values.password}
+                                    onChange={(e) => {
+                                      setFieldValue("password", e.target.value);
+                                      setFieldTouched("password", true);
+                                    }}
+                                    className="border border-gray-300 rounded-md p-3 w-full"
+                                  />
+                                  {values.password && (
+                                    <div className="absolute right-3 bottom-2 p-1 bg-gray-100 cursor-pointer">
+                                      {showPassword ? (
+                                        <div
+                                          className="flex items-center gap-1"
+                                          onClick={() =>
+                                            setShowPassword(!showPassword)
+                                          }
+                                        >
+                                          Ẩn <EyeOff />
+                                        </div>
+                                      ) : (
+                                        <div
+                                          className="flex items-center gap-1"
+                                          onClick={() =>
+                                            setShowPassword(!showPassword)
+                                          }
+                                        >
+                                          Hiện <Eye />
+                                        </div>
+                                      )}
+                                    </div>
+                                  )}
+                                </div>
+                              )}
+                            </div>
+                            <div className="flex justify-center">
+                              {!showPasswordBox && (
+                                <Button
+                                  variant="primary"
+                                  fill
+                                  disabled={!values.email}
+                                  onClick={() => {
+                                    setShowPasswordBox(true);
+                                    setFieldValue("password", "");
+                                  }}
+                                >
+                                  Tiếp tục
+                                </Button>
+                              )}
+                              {showPasswordBox && (
+                                <Button
+                                  type="submit"
+                                  variant="primary"
+                                  fill
+                                  disabled={!values.password}
+                                >
+                                  Đăng nhập
+                                </Button>
+                              )}
+                            </div>
                           </div>
                         </DialogContent>
                       );
